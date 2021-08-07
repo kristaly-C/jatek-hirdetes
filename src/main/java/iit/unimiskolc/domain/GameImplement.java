5236ce153b33e60fe6c3c006a57ef036fe9ed282
kristaly-C <tehen19@gmail.com>
2021-08-07 16:17:22 +0200

    #3 -Hirdetés megjelenítés készen  -Thymeleaf hozzáadva -Főoldalról elérhetőek mostantól a hírdetések -Működöképes szűrő a főoldalon

--- src/main/java/iit/unimiskolc/domain/GameImplement.java
+++ src/main/java/iit/unimiskolc/domain/GameImplement.java
@@ -99,4 +99,5 @@ public class GameImplement implements Game {
                 ", keyActivationSite=" + keyActivationSite +
                 '}';
     }
+
 }
---
+++ src/main/resources/static/js/newindexJs.js
@@ -0,0 +1,46 @@
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+    const myElement = document.getElementById('lista');
+    const ker = document.getElementById('#sea');
+
+    function kereso(){
+        var kiv = document.getElementById('sea');
+        var nev = kiv.value.toLowerCase();
+        console.log(nev);
+        for (let i = 0; i < myElement.children.length; i++) {
+            if (!myElement.children[i].children[1].children[0].textContent.toLowerCase().match(nev)){
+                myElement.children[i].classList.add("hide");
+                console.log("Kéne");
+            }else {
+                console.log("aaaaaaaaaa");
+                if (myElement.children[i].classList.contains('hide')){
+                    myElement.children[i].classList.remove('hide');
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
